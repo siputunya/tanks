176301c4dbc2f93018c472189c2a25eaa7fc0a05
Лизгунов Дмитрий <siputunya@mail.ru>
2023-12-27 04:13:53 +0300

    feat: innit commit

---
+++ frontend/src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1 className='app__title'>Тест</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/routers/index.js
@@ -0,0 +1,16 @@
+const Routers = {
+    DEFAULT: {
+        path: "/",
+        title: "Главная",
+    },
+    HOME: {
+        path: "/home",
+        title: "Главная",
+    },
+    NOT_FOUND: {
+        path: "*",
+        title: "Ошибка 404",
+    },
+}
+
+export default Routers
