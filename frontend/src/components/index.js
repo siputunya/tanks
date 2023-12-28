073eba95d9910199e9d8a604ed148f71a48363f2
Лизгунов Дмитрий <siputunya@mail.ru>
2023-12-28 23:03:46 +0300

    feat(index): Добавление в индексацию компонентов
    Добавлены компоненты Main, Footer и Header в индексацию проекта.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,4 @@
 export { default as App } from './App/App'
+export { default as Header } from './Header/Header'
+export { default as Main } from './Main/Main'
+export { default as Footer } from './Footer/Footer'
