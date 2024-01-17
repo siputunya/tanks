5ae1df235c2b89f4395c75b0c4b38921f056f648
Лизгунов Дмитрий <siputunya@mail.ru>
2024-01-18 02:54:46 +0300

    feat(variables): добавление статичных переменных

---
+++ frontend/src/utils/variables/variables.js
@@ -0,0 +1,21 @@
+import vkIcon from '@assets/vkIcon.svg'
+import tgIcon from '@assets/telegramIcon.svg'
+import whIcon from '@assets/whIcon.svg'
+
+export const SOCIAL_LINK = [
+  {
+    name: 'VK',
+    link: 'https://vk.com/#',
+    icon: vkIcon,
+  },
+  {
+    name: 'Telegram',
+    link: 'https://t.me/#',
+    icon: tgIcon,
+  },
+  {
+    name: 'whatsapp',
+    link: 'https://whatsapp.com/#',
+    icon: whIcon,
+  },
+]
